955e306bb964af347f780b197ec44ec20109d3b4
Crowdbotics <team@crowdbotics.com>
2020-11-08 03:36:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/rock_evolution_sv_22394/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rock_evolution_sv_22394;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "rock_evolution_sv_22394";
+    }
+}
---
+++ ios/rock_evolution_sv_22394Tests/rock_evolution_sv_22394Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface rock_evolution_sv_22394 : XCTestCase
+
+@end
+
+@implementation rock_evolution_sv_22394
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
